5cb870ae692bb2ba040a3ca695977dd1d1e96dc4
Magdiel Guillermo Valdez <magdiel.gvdz@gmail.com>
2024-03-13 13:59:08 -0600

    add apps

--- server/books/models.py
+++ server/books/models.py
@@ -4,13 +4,10 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    pubdate = models.DateField()
     isbn = models.CharField(max_length=100)
-    language = models.CharField(max_length=100)
     barcode = models.CharField(max_length=16)
     stock = models.IntegerField()
     #author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    #category = models.ForeignKey(Category, on_delete=models.CASCADE)
     #editorial = models.ForeignKey(Editorial, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -18,6 +15,8 @@ class Book(models.Model):
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
--- server/users/models.py
+++ server/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class user(models.Model):
+    name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    password = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    status = models.BooleanField(default=False)
+    register_date = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField()
+    def __str__(self):
+        return self.username
