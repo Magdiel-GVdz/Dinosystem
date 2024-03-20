215a9af7c93ac5cfe520e5f48130de7d230314a9
E-Agustin <agus.e.hhh@gmail.com>
2024-03-20 10:33:36 -0600

    add login

---
+++ client/src/Button.jsx
@@ -0,0 +1,17 @@
+import PropTypes from "prop-types";
+import "./Boton.css";
+
+export function Button({ text }) {
+  console.log(text);
+  return (
+    <button
+      className="boton"
+      style={{ position: "absolute", top: "600px", left: "70px" }}
+    >
+      {text}
+    </button>
+  );
+}
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+};
---
+++ client/src/Fondo.jsx
@@ -0,0 +1,6 @@
+export function Fondo(){
+return <div style={{ width: '100%', height: '100vh', backgroundColor: '#0C130C' }}>
+
+</div>
+
+}
