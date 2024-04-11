0ce54d78bd8fb8e3af6b3050548876dd3008fa80
Magdiel Guillermo Valdez <magdiel.gvdz@gmail.com>
2024-04-11 17:09:32 -0600

    add link to sidebar

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import { Menu, MenuItem, Sidebar } from "react-pro-sidebar";
+import { Link } from "react-router-dom";
 
 const SideBar = () => {
   return (
     <div style={{ display: "flex", height: "100%", minHeight: "400px" }}>
       <Sidebar>
         <Menu>
-          <MenuItem>DinoVentas </MenuItem>
-          <MenuItem>DinoCompras </MenuItem>
-          <MenuItem>DinoLibros </MenuItem>
-          <MenuItem>DinoPromociones </MenuItem>
-          <MenuItem>DinoMermas </MenuItem>
-          <MenuItem>DinoReporte </MenuItem>
-          <MenuItem>DinoDonaciones </MenuItem>
-          <MenuItem>DinoDevoluciones </MenuItem>
-          <MenuItem>DinoUsuarios </MenuItem>
+          <MenuItem component={<Link to="/ventas" />}>DinoVentas</MenuItem>
+          <MenuItem component={<Link to="/compras" />}>DinoCompras</MenuItem>
+          <MenuItem component={<Link to="/libros" />}>DinoLibros</MenuItem>
+          <MenuItem component={<Link to="/promociones" />}>DinoPromociones</MenuItem>
+          <MenuItem component={<Link to="/mermas" />}>DinoMermas</MenuItem>
+          <MenuItem component={<Link to="/reporte" />}>DinoReporte</MenuItem>
+          <MenuItem component={<Link to="/donaciones" />}>DinoDonaciones</MenuItem>
+          <MenuItem component={<Link to="/devoluciones" />}>DinoDevoluciones</MenuItem>
+          <MenuItem component={<Link to="/usuarios" />}>DinoUsuarios</MenuItem>
+
           <MenuItem>Cerrar sesion </MenuItem>
         </Menu>
       </Sidebar>
