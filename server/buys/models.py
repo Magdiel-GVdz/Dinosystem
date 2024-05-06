070b9465de7af163b5a7e65bd96fc25a0fd7793a
Magdiel Guillermo Valdez <magdiel.gvdz@gmail.com>
2024-05-05 21:07:28 -0600

    fix buys

--- server/buys/models.py
+++ server/buys/models.py
@@ -1,43 +1,18 @@
 from django.db import models
 from users.models import User
 from books.models import Book
-
+from promos.models import Promotion
+from django.utils import timezone
 
 class Buy(models.Model):
-    user = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     date = models.DateTimeField(auto_now_add=True)
-    total_price = models.CharField(max_length=250, blank=True)
+    total_price = models.FloatField()
     
 class BuyItem(models.Model):
     buy = models.ForeignKey(Buy, on_delete=models.SET_NULL, null=True)
-    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+    book = models.ForeignKey(Book, on_delete=models.SET_NULL, null=True)
     quantity = models.IntegerField(default=0)
-    price = models.CharField(max_length=250, blank=True)
-    subtotal = models.FloatField()
-    returned = models.BooleanField(default=False)
+    price = models.FloatField()
     
-# Create your models here.
-# ejemplo de peticion para la api
-# {
-#     'user',
-#     'detalleVenta' : [
-#         {
-#             'libro',
-#             'promocion',
-#             'cantidad',
-#             'precio',
-#             'subtotal'
-#             'devuelto'
-#         },
-#         {
-#             'libro',
-#             'promocion',
-#             'cantidad',
-#             'precio',
-#             'subtotal'
-#             'devuelto'
-#         }
-#     ]
-#     'fecha',
-#     'total'   
-# }
+ 
