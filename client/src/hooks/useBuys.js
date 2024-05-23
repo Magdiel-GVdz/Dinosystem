be2614277491cb33073ed5da2fb6757c259df94a
BrendanAlxndr8 <brendanalexander66@gmail.com>
2024-05-23 12:21:55 -0500

    Cambios en los Hooks

--- client/src/hooks/useBuys.js
+++ client/src/hooks/useBuys.js
@@ -17,10 +17,59 @@ export const useBuys = () => {
             }
             return data
         } catch (error) {
-            console.error("Error getting promos:", error)
+            console.error("Error getting buys:", error)
+            return []
+        }
+    }
+
+    const getBuy = async (id) => {
+
+        try {
+            const response = await axios.get(`http://localhost:8000/api/v1/buys/${id}`)
+            const { data } = response || {}
+            return data || {}
+        } catch (error) {
+            console.error(`Error getting buy with id ${id}:`, error)
+            return {}
+        }
+    }
+
+    const createBuy = async (data) => {
+
+        try {
+            const response = await axios.post("http://localhost:8000/api/v1/buys/create/", data)
+            const { data: serverData } = response || {}
+            if (!serverData) {
+                console.error("Unexpected null data from server")
+                return []
+            }
+            return serverData
+        } catch (error) {
+            console.error("Error creating buy:", error)
+            return []
+        }
+    }
+
+
+    const myBuys = async () => {
+
+        try {
+            const response = await axios.get("http://localhost:8000/api/v1/buys/my")
+            if (!response) {
+                console.error("Unexpected null response from server")
+                return []
+            }
+            const { data } = response
+            if (!data) {
+                console.error("Unexpected null data from server")
+                return []
+            }
+            return data
+        } catch (error) {
+            console.error("Error getting my buys:", error)
             return []
         }
     }
     
-    return { getBuys }
+    return { getBuys, getBuy, createBuy, myBuys }
 }
